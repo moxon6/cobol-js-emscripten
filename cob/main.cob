2020954acccde59a8aaef227d3b7a202350422f0
Martin Moxon <moxon6@googlemail.com>
2020-05-10 21:09:22 +0000

    Simplify

--- cob/main.cob
+++ cob/main.cob
@@ -4,21 +4,14 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
 
-       WORKING-STORAGE SECTION.
-       01 HELLO PIC X(6) VALUE "Hello ".
-       01 WORLD PIC X(6).
-       01 A PIC 999.
-       
+       WORKING-STORAGE SECTION.       
        01 UpdateColor PIC X(60) VALUE z"document.body.style.backgroundColor = 'rgb(120,0,0)'".
-
-       PROCEDURE DIVISION.
-
-       PERFORM UPDATE-COLOR.
-
        
-       DISPLAY HELLO WORLD.
+       PROCEDURE DIVISION.
+       PERFORM UPDATE-COLOR.
+       
        STOP RUN.
-
+       
        UPDATE-COLOR. 
        CALL "jsrun" USING UpdateColor
        CALL "domSleep".
