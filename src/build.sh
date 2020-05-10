cef685c8cbfe5f04c849fc7408a7d99d42338352
Martin Moxon <moxon6@googlemail.com>
2020-05-10 17:32:23 +0000

    Remove unused files

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,21 @@
+mkdir -p tmp
 mkdir -p out
-cobc -v -free -x -o out/helloworld helloworld.cob
+
+cobc -C -x -free hello.cob -o tmp/hello.c
+
+# Concat say with tmp/hello.c
+rm /tmp/hello.c
+mv tmp/hello.c /tmp/hello.c
+cat say.c /tmp/hello.c > tmp/hello.c
+
+
+# Replace dynamic logic for direct function pointers
+# cat say.c >> tmp/hello.c
+sed -i -r "s/cob_resolve_cobol\ \(\"(.*)\",.*\);/\*\1;/g" tmp/hello.c
+
+emcc --ignore-dynamic-linking -o out/hello.html tmp/hello.c \
+  /root/opt/lib/*.a -I/root/opt/include \
+  -I/workspaces/cobol-wasm-hello-world/gnucobol-3.0-rc1 -s FORCE_FILESYSTEM=1 \
+  -s NO_EXIT_RUNTIME=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s ASYNCIFY
+
+sed -i '/To use dlopen/s/^/\/\//' out/hello.js # Comment out all dlopen callbacks
--- src/new-build.sh
+++
@@ -1,8 +0,0 @@
-cobc -C -x -free hello.cob -o tmp/hello.c
-rm /tmp/hello.c
-mv tmp/hello.c /tmp/hello.c
-cat say.c /tmp/hello.c > tmp/hello.c
-
-clang -S -emit-llvm -o "./tmp/hello.bc" "tmp/hello.c"
-clang  -o "hello" "./tmp/hello.bc" \
-   -L/usr/local/lib -lcob -lm -lgmp -ldl
